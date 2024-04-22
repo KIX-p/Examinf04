6b1ec6ebb4925d5ced783a6da6c11d26571871a1
Pawel Kikh <pavlokiht2005@gmail.com>
2024-04-22 19:01:59 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+
+function Body() {
+  const [title, setTitle] = useState("");
+  const [genre, setGenre] = useState("");
+  function handleTitleChange(event) {
+    setTitle(event.target.value);
+  }
+
+  function handleGenreChange(event) {
+    setGenre(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Tytuł: ", title);
+    console.log("Rodzaj: ", genre);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Tytuł filmu:
+        <br />
+        <input type="text" name="title" onChange={handleTitleChange} />
+      </label>
+      <br />
+      <label>
+        Rodzaj filmu:
+        <br />
+        <select onChange={handleGenreChange}>
+          <option></option>
+          <option value="1">komedia</option>
+          <option value="2">Obyczajowy</option>
+          <option value="3">Sensacyjny</option>
+          <option value="4">Horror</option>
+        </select>
+      </label>
+      <br />
+      <input type="submit" value="Wyślij" className="btn btn-primary"/>
+
+    </form>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="body">
+        <Body />
+      </div>
+    </>
+  )
 }
 
 export default App;
